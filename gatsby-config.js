8bc61587d19b76bc941617110f6fa0b4388e52ad
Selena Siri <selenasiri@gmail.com>
2020-05-26 12:39:05 -0400

    init

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "NJWWA123",
+    description:
+      "Explore awesome windsurf & watersport what makes each of them unique. forget your daily routine & say yes to adventure in water",
+    author: "njwwa",
+    twitterUsername: "@njwwa",
+    image: "defaultBcg.png",
+    siteUrl: "http://windsurfnj.org/",
+  },
+  /* Your site config here */
+  plugins: [],
+}
